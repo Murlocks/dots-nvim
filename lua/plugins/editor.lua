8c091ff9d1a1d27d16e18b6898ea5575f8572402
Rene Tianco <mysterio2004173@gmail.com>
2025-05-11 00:53:47 -0400

    Updates

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -52,28 +52,37 @@ return {
     },
   },
   {
-    "nvim-treesitter/playground",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        playground = {
-          enable = true,
-          disable = {},
-          updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-          persist_queries = false, -- Whether the query persists across vim sessions
-          keybindings = {
-            toggle_query_editor = "o",
-            toggle_hl_groups = "i",
-            toggle_injected_languages = "t",
-            toggle_anonymous_nodes = "a",
-            toggle_language_display = "I",
-            focus_language = "f",
-            unfocus_language = "F",
-            update = "R",
-            goto_node = "<cr>",
-            show_help = "?",
-          },
-        },
-      })
-    end,
+    "echasnovski/mini.align",
+    version = false,
+    keys = {
+      { "ga", desc = "Start align", mode = { "n", "v" } },
+      { "gA", desc = "Start align with preview", mode = { "n", "v" } },
+    },
+    config = true,
   },
+  -- {
+  --   "nvim-treesitter/playground",
+  --   config = function()
+  --     require("nvim-treesitter.configs").setup({
+  --       playground = {
+  --         enable = true,
+  --         disable = {},
+  --         updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+  --         persist_queries = false, -- Whether the query persists across vim sessions
+  --         keybindings = {
+  --           toggle_query_editor = "o",
+  --           toggle_hl_groups = "i",
+  --           toggle_injected_languages = "t",
+  --           toggle_anonymous_nodes = "a",
+  --           toggle_language_display = "I",
+  --           focus_language = "f",
+  --           unfocus_language = "F",
+  --           update = "R",
+  --           goto_node = "<cr>",
+  --           show_help = "?",
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
 }
